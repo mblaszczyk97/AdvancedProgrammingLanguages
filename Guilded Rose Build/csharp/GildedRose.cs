b72aeb5ba965af430ab13504767929be1b2f344c
Mikołaj Błaszczyk <mikolajbl97@gmail.com>
2019-10-09 10:46:33 +0200

    added build C#

---
+++ Guilded Rose Build/csharp/GildedRose.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+
+namespace csharp
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        public void UpdateQuality()
+        {
+
+            for (var i = 0; i < Items.Count; i++)
+            {
+                if (Items[i].Name != "Aged Brie" && !Items[i].Name.ToLower().Contains("backstage passes") && !Items[i].Name.ToLower().Contains("conjured"))//Added Conjured item check
+                {
+                    if (Items[i].Quality > 0)
+                    {
+                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            Items[i].Quality = Items[i].Quality - 1;
+                        }
+                    }
+                }
+                else
+                {
+                    if (Items[i].Quality < 50)
+                    {
+                        Items[i].Quality = Items[i].Quality + 1;
+
+                        if (Items[i].Quality > 2 && Items[i].Name.ToLower().Contains("conjured"))//Conjured item decreases quality -2
+                        {
+                            Items[i].Quality = Items[i].Quality - 3;
+                        }
+
+                        if (Items[i].Name.ToLower().Contains("backstage passes"))
+                        {
+                         
+
+                            if (Items[i].SellIn < 11)
+                            {
+                                if (Items[i].Quality < 50)
+                                {
+                                    Items[i].Quality = Items[i].Quality + 1;
+                                }
+                            }
+
+                            if (Items[i].SellIn < 6)
+                            {
+                                if (Items[i].Quality < 50)
+                                {
+                                    Items[i].Quality = Items[i].Quality + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    Items[i].SellIn = Items[i].SellIn - 1;
+                }
+
+                if (Items[i].SellIn < 0)
+                {
+                    if (Items[i].Name != "Aged Brie")
+                    {
+                        if (!Items[i].Name.ToLower().Contains("backstage passes"))
+                        {
+                            if (Items[i].Name.ToLower().Contains("conjured") && Items[i].Quality > 1) //Conjured item decreases twice as fast so its 2*-2=-4
+                            {
+                                Items[i].Quality = Items[i].Quality - 1;
+                            }
+                            if (Items[i].Quality > 0)
+                            {
+                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                {
+                                    Items[i].Quality = Items[i].Quality - 1;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                        }
+                    }
+                    else
+                    {
+                        if (Items[i].Quality < 50)
+                        {
+                            Items[i].Quality = Items[i].Quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
